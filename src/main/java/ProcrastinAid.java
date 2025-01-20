84f3a104131daf11f3ea5bfa97a7609e2d7c177e
nick.t <52658893+nickt121@users.noreply.github.com>
2025-01-20 23:12:24 +0800

    feat: refactor greeting and bye into individual methods

--- src/main/java/ProcrastinAid.java
+++ src/main/java/ProcrastinAid.java
@@ -1,7 +1,15 @@
 public class ProcrastinAid {
     public static void main(String[] args) {
+        hello();
+        bye();
+    }
+
+    public static void hello(){
         System.out.println("Hello! I'm ProcrastinAid");
         System.out.println("What can I do for you?\n");
+    }
+
+    public static void bye(){
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
