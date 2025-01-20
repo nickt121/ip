9aee0d0f9d79a4b1df12dc6c6b347a053c6effda
nick.t <52658893+nickt121@users.noreply.github.com>
2025-01-21 01:27:24 +0800

    fix: string formating

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,7 +10,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return super.toString() + " (from: " + this.startDate + "to:" + this.endDate + " )";
+        return super.toString() + " (from: " + this.startDate + " to: " + this.endDate + " )";
     }
 
     @Override
