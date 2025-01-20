adca0fd623d92f8cb5150c2703d7bb942ec8c2d6
nick.t <52658893+nickt121@users.noreply.github.com>
2025-01-20 22:49:32 +0800

    feat: refactored Duke to ProcrastinAid

---
+++ src/main/java/ProcrastinAid.java
@@ -0,0 +1,7 @@
+public class ProcrastinAid {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ProcrastinAid");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
