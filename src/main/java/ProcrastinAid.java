bec4db2f4cffe944e0a7f7192e60253a9ad36e9c
nick.t <52658893+nickt121@users.noreply.github.com>
2025-01-20 23:41:33 +0800

    feat: added feature to add to and view tasks in a list

--- src/main/java/ProcrastinAid.java
+++ src/main/java/ProcrastinAid.java
@@ -1,28 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProcrastinAid {
+    public static ArrayList<String> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         hello();
         Scanner userInput = new Scanner(System.in);
-        while(true){
+
+        while (true) {
             String inp = getInput(userInput);
-            if (inp.equals("bye")){break;}
-            System.out.println(inp + "\n");
+            if (inp.equals("bye")) {
+                break;
+            } else if (inp.equals("list")) {
+                printTasks();
+            } else {
+                addTask(inp);
+            }
         }
         bye();
     }
 
-    public static void hello(){
+    public static void hello() {
         System.out.println("Hello! I'm ProcrastinAid");
         System.out.println("What can I do for you?\n");
     }
 
-    public static void bye(){
+    public static void bye() {
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 
-    public static String getInput(Scanner userInput){
+    public static String getInput(Scanner userInput) {
         System.out.print("> ");
         return userInput.nextLine();
+    }
+
+    public static void addTask(String task) {
+        taskList.add(task);
+    }
+
+    public static void printTasks() {
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(String.valueOf(i) + ' ' + taskList.get(i));
+        }
     }
 }
