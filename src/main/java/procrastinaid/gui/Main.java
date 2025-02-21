26faea9a775a07baf57104004e2c05a58a3cdfb7
nick.t <52658893+nickt121@users.noreply.github.com>
2025-02-21 16:17:11 +0800

    feat: add icon and title bar

--- src/main/java/procrastinaid/gui/Main.java
+++ src/main/java/procrastinaid/gui/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import procrastinaid.ui.ProcrastinAid;
@@ -22,6 +23,9 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("ProcrastinAid");
+            Image icon = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+            stage.getIcons().add(icon);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setChatbot(procrastinaidBot);
             stage.show();
